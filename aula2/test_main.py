0affe876640de0ac911de0279ec0af8f19cef157
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-10-10 13:45:57 -0300

    test

--- aula2/test_main.py
+++ aula2/test_main.py
@@ -1,16 +1,43 @@
 from Coordinate import Coordinate
 from menu import Menu
-import program
-from quadrant import Quadrant
 
 
-def test_menu_test():
+
+def test_get_user_coordinate():
     a = 3
     b = 4
-    menu = coordinates(a,b)
+    menu = Coordinate(a,b)
     
-    result = menu.show_menu()
+    result = menu.get_user_coordinate()
     
     assert result == 3,4
     
-def test_
+def test_cordinate_is_valid():
+    a = 1
+    b = 0
+    test = Coordinate(a, b)
+    
+    result = test.coordinate_is_valid(a,b)
+    
+    assert result == False
+    
+
+def test_cordinate_is_valid1():
+    a = 0
+    b = 1
+    test = Coordinate(a, b)
+    
+    result = test.coordinate_is_valid(a,b)
+    
+    assert result == False
+    
+def test_cordinate_is_valid2():
+    a = 3
+    b = 1
+    test = Coordinate(a, b)
+    
+    result = test.coordinate_is_valid(a,b)
+    
+    assert result == True
+    
+    
--- aula2/test_quadrant.py
+++ aula2/test_quadrant.py
@@ -14,3 +14,45 @@ def test_should_get_quadrant_coordinate():
 
     # Assert
     assert result == "First"
+    
+
+def test_should_get_quadrant_coordinate1():
+    # Arrange / Setup
+    a = -10
+    b = 20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Second"
+    
+    
+def test_should_get_quadrant_coordinate2():
+    # Arrange / Setup
+    a = 10
+    b = -20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Fourth"
+    
+
+def test_should_get_quadrant_coordinate3():
+    # Arrange / Setup
+    a = -10
+    b = -20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Third"
